d0d8a69128d4a27ab4a948819da5911870f5ab52
Dorian <dorianmeade1@gmail.com>
2020-06-12 14:11:08 -0400

    just some stuff that was not my task, but was within a tutorial I was following. do what you please with it.

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("register", views.register, name="register"),
     path("login", views.login_request, name="login"),
+    path("logout", views.logout_request, name="logout"),
     path("", views.filter_request)
 ]
