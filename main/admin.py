114d191052a5c8278058f70d5978e940d0a7e97a
Konstantin Kerekovski <kerekovskik@gmail.com>
2020-06-18 17:44:10 -0400

    Register models to admin portal

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
+from .models import Card_Rarity,Card_Type,Card_Color,Card,Location,Seller,Bazaar_User,Listing,Notification
 
 # Register your models here.
-
+admin.site.register(Card_Rarity)
+admin.site.register(Card_Type)
+admin.site.register(Card_Color)
+admin.site.register(Card)
+admin.site.register(Location)
+admin.site.register(Seller)
+admin.site.register(Bazaar_User)
+admin.site.register(Listing)
+admin.site.register(Notification)
