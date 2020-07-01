189f0b6f0492cac382ca01edcb8278d2de55cb65
Konstantin Kerekovski <kerekovskik@gmail.com>
2020-07-01 15:49:20 -0400

    modify .gitignore to reflect pypi project name files to be excluded add Django to requirements.txt modify setup.py to prepare for pypi uploads change entry point for app to be bazaar_start

--- setup.py
+++ setup.py
@@ -1,18 +1,32 @@
 from setuptools import setup
 
-setup(
-    name='bazaar_of_wonders',
-    version='0.0.0.1',
-    packages=['bazaar_of_wonders','main','bazaar_of_wonders_server_scripts','bazaar_of_wonders_db'],
-    description='Sample Django pip Project',
-    install_requires=['django-tinymce4-lite','django-materializecss-form','PyYAML'],
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
+setup(
+    name='django-bazaar-of-wonders',
+    author='https://github.com/Bazaar-Trader',
+    version='0.1.3',
+    
+    description='Bazaar of Wonders is a Django-based web application that aggregates card details and pricing statistics for trading card games, namely, Magic: The Gathering.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    
+    url="https://github.com/Bazaar-Trader/Bazaar_Of_Wonders",
+    packages=['bazaar_of_wonders','main','bazaar_of_wonders_server_scripts','bazaar_of_wonders_db'],
+    include_package_data=True,
+    install_requires=['django','django-tinymce4-lite','django-materializecss-form','PyYAML'],
+    python_requires='>=3.8',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    
     entry_points =
     { 'console_scripts':
         [
-            'runmyserver = bazaar_of_wonders_server_scripts.run:main',
-#            'initmyserver = bazaar_of_wonders_server_scripts.init:main',
+            'bazaar_start = bazaar_of_wonders_server_scripts.run:main',
         ]
     },
-    include_package_data=True
+    
 )
