8a9f670582314f9296cf68c2184daea529a466d9
dorianmeade <dorianmeade@ufl.edu>
2020-07-19 15:55:55 -0400

    init background function to email notify user

--- bazaar_of_wonders/settings.py
+++ bazaar_of_wonders/settings.py
@@ -35,8 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tinymce',
-    'materializecssform'
+    'materializecssform',
+    'background_task',
 ]
 
 EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
